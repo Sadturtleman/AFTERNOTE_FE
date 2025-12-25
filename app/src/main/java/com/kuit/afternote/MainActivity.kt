a30944cc6e7d64ae17c92424b7e3fe82e7fd5fe5
Zach <dnfjddk2@gmail.com>
2025-12-25 16:14:08 +0900

    feat: update MainActivity to use AfternoteMainScreen and add new color definitions

--- app/src/main/java/com/kuit/afternote/MainActivity.kt
+++ app/src/main/java/com/kuit/afternote/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kuit.afternote.feature.mainpage.presentation.screen.AfternoteMainScreen
 import com.kuit.afternote.ui.theme.AfternoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AfternoteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
+                    AfternoteMainScreen(
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
