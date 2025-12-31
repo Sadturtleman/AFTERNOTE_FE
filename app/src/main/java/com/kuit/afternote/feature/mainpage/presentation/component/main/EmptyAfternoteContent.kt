38e1f82590c885700843bedf8ad6adbc61f16699
1hyok <dnfjddk2@gmail.com>
2025-12-31 19:20:58 +0900

    chore: EmptyAfternoteContent 수정사항 반영

--- app/src/main/java/com/kuit/afternote/feature/mainpage/presentation/component/main/EmptyAfternoteContent.kt
+++ app/src/main/java/com/kuit/afternote/feature/mainpage/presentation/component/main/EmptyAfternoteContent.kt
@@ -3,6 +3,7 @@ package com.kuit.afternote.feature.mainpage.presentation.component.main
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
