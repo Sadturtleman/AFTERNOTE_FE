209edb903dcba39f256ecbc41fce9401bde00aff
1hyok <dnfjddk2@gmail.com>
2026-01-01 21:56:02 +0900

    fix: AfternoteDetailScreen unused import 제거

--- app/src/main/java/com/kuit/afternote/feature/mainpage/presentation/screen/AfternoteDetailScreen.kt
+++ app/src/main/java/com/kuit/afternote/feature/mainpage/presentation/screen/AfternoteDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.kuit.afternote.feature.mainpage.presentation.screen
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
