c410b590702093359542cd318221f87c02b6ef3f
Sadturtleman <rugsn12345@gmail.com>
2026-01-08 23:43:43 +0900

    feat: verify screen add

--- app/src/main/java/com/kuit/afternote/core/uimodel/Step.kt
+++ app/src/main/java/com/kuit/afternote/core/uimodel/Step.kt
@@ -1,6 +1,7 @@
 package com.kuit.afternote.core.uimodel
 
 interface Step {
-    val value : Int
-    fun previous() : Step?
+    val value: Int
+
+    fun previous(): Step?
 }
--- app/src/main/java/com/kuit/afternote/feature/receiver/presentation/uimodel/VerifyStep.kt
+++ app/src/main/java/com/kuit/afternote/feature/receiver/presentation/uimodel/VerifyStep.kt
@@ -4,14 +4,13 @@ import com.kuit.afternote.core.uimodel.Step
 
 enum class VerifyStep(
     override val value: Int
-): Step {
+) : Step {
     EMAIL_AUTH(1),
     MASTER_KEY_AUTH(2),
     UPLOAD_PDF_AUTH(3),
     END(4);
 
-    override
-    fun previous(): VerifyStep? =
+    override fun previous(): VerifyStep? =
         when (this) {
             EMAIL_AUTH -> null
             MASTER_KEY_AUTH -> EMAIL_AUTH
