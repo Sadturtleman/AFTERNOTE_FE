845467cd5edfe8eddd4eda5861bc7750e9aa4605
Sadturtleman <rugsn12345@gmail.com>
2026-01-23 11:07:02 +0900

    chore: import modify

--- app/src/main/java/com/kuit/afternote/feature/onboarding/presentation/screen/SignUpScreen.kt
+++ app/src/main/java/com/kuit/afternote/feature/onboarding/presentation/screen/SignUpScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -24,7 +23,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.kuit.afternote.core.ui.component.EmailInputContent
 import com.kuit.afternote.core.ui.component.SignUpContentButton
 import com.kuit.afternote.core.ui.component.TopBar
 import com.kuit.afternote.feature.onboarding.presentation.component.IdentifyInputContent
