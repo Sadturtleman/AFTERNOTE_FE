c9d82524b74081b2d1e5f492c6744c0930eaa569
1hyok <dnfjddk2@gmail.com>
2026-02-03 14:13:52 +0900

    refactor(timeletter): reorganize import paths for navigation components

--- app/src/main/java/com/kuit/afternote/feature/timeletter/presentation/screen/ReceiveListScreen.kt
+++ app/src/main/java/com/kuit/afternote/feature/timeletter/presentation/screen/ReceiveListScreen.kt
@@ -23,8 +23,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kuit.afternote.R
-import com.kuit.afternote.core.BottomNavItem
-import com.kuit.afternote.core.BottomNavigationBar
+import com.kuit.afternote.core.ui.component.navigation.BottomNavItem
+import com.kuit.afternote.core.ui.component.navigation.BottomNavigationBar
 import com.kuit.afternote.feature.timeletter.data.dto.TimeLetterReceiver
 import com.kuit.afternote.feature.timeletter.presentation.component.ReceiverInfoItem
 import com.kuit.afternote.feature.timeletter.presentation.component.chosungGroupedItems
